9f3741d365cfec5d221b0e2d7ff18b28a1587dab
Onyinye Uche <onyinye@paystack.com@Onyinyes-MacBook-Pro.local>
2024-06-11 04:51:25 +0100

    remove open telemetry config

--- node_modules/@opentelemetry/instrumentation-undici/build/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { UndiciInstrumentation } from './undici';
-export { UndiciRequest, UndiciResponse, IgnoreRequestFunction, RequestHookFunction, StartSpanHookFunction, UndiciInstrumentationConfig, } from './types';
-//# sourceMappingURL=index.d.ts.map
